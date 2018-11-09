dd2fd5795d6f19bdf2d1a59895686a326e9da859
Michael Trinh <mptrinh@cpp.edu>
2018-11-08 23:13:49 -0800

    added both functions

--- RootLocator.java
+++ RootLocator.java
@@ -13,12 +13,32 @@
  * @author Michael Trinh
  */
 public class RootLocator {
-
+    
+     // f1(x) = 2x^3 – 11.7x^2 + 17.7x – 5;
+    double getFunction1OfX(double x) {
+        return ((2 * Math.pow(x, 3)) - (11.7 * Math.pow(x, 2)) + (17.7 * x) - 5);
+    }
+    
+    // f2(x) = x + 10 – xcosh(50/x)
+    double getFunction2OfX(double x) {
+        if (x == 0) throw new IllegalArgumentException("Divide by zero error.");
+        return (x + 10 - (x * Math.cosh(50 / x)));
+    }
+    
+    public void doBisectionMethod(String function, double leftInterval, 
+            double rightInterval) {
+        
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("Michael Trinh's Root Locator");
+        RootLocator rootLocator = new RootLocator();
+        double x = 0;
+        System.out.println("F1(" + x + ") = " + rootLocator.getFunction1OfX(x));
+        System.out.println("F2(" + x + ") = " + rootLocator.getFunction2OfX(x));
     }
     
 }
