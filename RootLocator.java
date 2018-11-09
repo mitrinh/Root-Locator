9979c6b8d8af2f7cb55c527e20bda7926891459d
Michael Trinh <mptrinh@cpp.edu>
2018-11-08 21:31:44 -0800

    added starting file

---
+++ RootLocator.java
@@ -0,0 +1,24 @@
+/**
+ * Michael Trinh
+ * CS 3010
+ * Programming Project 2
+ * Description: Write a program that asks the user for the number of linear
+ *              equations to solve using 3 methods. Write programs for all
+ *              the methods (Bisection, Newton-Raphson, Secant, False-Position 
+ *              and Modified Secant) for locating roots.
+ */
+
+/**
+ *
+ * @author Michael Trinh
+ */
+public class RootLocator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Michael Trinh's Root Locator");
+    }
+    
+}
