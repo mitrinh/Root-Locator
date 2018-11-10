66475f0fd38130ead04ca1f1ad3374c18836ea5a
Michael Trinh <mptrinh@cpp.edu>
2018-11-10 01:12:44 -0800

    added bisection method

--- RootLocator.java
+++ RootLocator.java
@@ -2,10 +2,9 @@
  * Michael Trinh
  * CS 3010
  * Programming Project 2
- * Description: Write a program that asks the user for the number of linear
- *              equations to solve using 3 methods. Write programs for all
- *              the methods (Bisection, Newton-Raphson, Secant, False-Position 
- *              and Modified Secant) for locating roots.
+ * Description: Write a program that locates the roots in equations using the 
+ *              methods: Bisection, Newton-Raphson, Secant, False-Position 
+ *              and Modified Secant.
  */
 
 /**
@@ -13,6 +12,10 @@
  * @author Michael Trinh
  */
 public class RootLocator {
+    
+    public boolean sameSign(double num1, double num2){
+        return ((num1 < 0) == (num2 < 0));
+    }
     
      // f1(x) = 2x^3 – 11.7x^2 + 17.7x – 5;
     double getFunction1OfX(double x) {
@@ -25,9 +28,35 @@ public class RootLocator {
         return (x + 10 - (x * Math.cosh(50 / x)));
     }
     
-    public void doBisectionMethod(String function, double leftInterval, 
-            double rightInterval) {
-        
+    public void doBisectionMethod(int function, double leftInterval, 
+            double rightInterval, double desiredError) {
+        // so that loop doesn't break
+        double approximateError = 1;
+        double currentRoot = (leftInterval + rightInterval) / 2;
+        double previousRoot;
+        double funRoot;
+        double funLeft;
+        for(int i = 0; approximateError < desiredError; i++) {
+            previousRoot = currentRoot;
+            currentRoot = (leftInterval + rightInterval) / 2;
+            if (i != 0) {
+                approximateError = Math.abs((currentRoot-previousRoot) / 
+                        currentRoot);
+            }
+            if (function == 1) {
+                funRoot = getFunction1OfX(currentRoot);
+                funLeft = getFunction1OfX(leftInterval);
+            }
+            else {
+                funRoot = getFunction2OfX(currentRoot);
+                funLeft = getFunction2OfX(leftInterval);
+            }
+            if (sameSign(funRoot, funLeft))
+                leftInterval = currentRoot;
+            else
+                rightInterval = currentRoot;
+        }   
+        System.out.println("Root found at x = " + currentRoot);
     }
     
     /**
@@ -36,9 +65,7 @@ public class RootLocator {
     public static void main(String[] args) {
         System.out.println("Michael Trinh's Root Locator");
         RootLocator rootLocator = new RootLocator();
-        double x = 0;
-        System.out.println("F1(" + x + ") = " + rootLocator.getFunction1OfX(x));
-        System.out.println("F2(" + x + ") = " + rootLocator.getFunction2OfX(x));
+        rootLocator.doBisectionMethod(1, 1, 5, .10);
     }
     
 }
